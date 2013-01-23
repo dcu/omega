b8faefe9486ba4589a2ede4d04c442e355d42a79
David A. Cuadrado <krawek@gmail.com>
2013-01-23 00:08:30 -0500

    Fix typo on boot.rb

--- lib/omega/generators/templates/skel/config/boot.rb
+++ lib/omega/generators/templates/skel/config/boot.rb
@@ -4,7 +4,7 @@ OMEGA_ROOT = File.expand_path('../..', __FILE__) unless defined?(OMEGA_ROOT)
 
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
-Bundler.require(:default, OMEGA)
+Bundler.require(:default, OMEGA_ENV)
 
 Omega.load!
 
